20d5dee5b213e7ac458c75e8501231c89a97ba80
lucasomac0 <sfslucas2005@gmail.com>
2023-05-26 22:11:01 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("criadoPor");
+    button.addEventListener("click", function() {
+      alert("Lucas Oliveira Macedo\nAriane Ferreira de Andrade\nNickolas Marmitt Murakami\nDisciplina de Sistemas Operacionais.\nProfessor: Paulo Manseira.");
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var button2 = document.getElementById("referencias");
+    button2.addEventListener("click", function() {
+      alert(" VAL, Carlos Eduardo do. Ubuntu Guia do Iniciante 2.0. Novatec Editora, 2016.\n  HELMKE, Matthew; HUDSON, Andrew; HUDSON, Paul. Ubuntu Unleashed. Indianapolis: Sams Publishing, 2021.\n 'Como criar uma máquina virtual VirtualBox'. Tecnoblog. Disponível em: https://tecnoblog.net/responde/como-criar-uma-maquina-virtual-virtualbox/. Acesso em: 22 de Maio de 2023.\n  MANSEIRA, Paulo. Aulas da matéria de Sistemas Operacionais. Católica de Santa Catarina, 2023.");
+    });
+  });
